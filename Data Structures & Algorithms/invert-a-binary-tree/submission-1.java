0d3378f9403f3b440fc321b5e4ea31eb75a34eec
PawanCodes <pawansinghb07@gmail.com>
2026-08-19 21:00:40 +0530

    Add: invert-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            TreeNode node = q.remove();
+
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+
+            if (node.left != null)
+                q.add(node.left);
+
+            if (node.right != null)
+                q.add(node.right);
+        }
+
+        return root;
+    }
+}
