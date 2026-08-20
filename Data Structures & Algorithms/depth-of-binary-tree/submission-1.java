aac6c66d0acf635e6929ea71d6245391302beb4a
PawanCodes <pawansinghb07@gmail.com>
2026-08-20 23:06:14 +0530

    Add: depth-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        int depth = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            depth++;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.remove();
+
+                if (node.left != null)
+                    q.add(node.left);
+
+                if (node.right != null)
+                    q.add(node.right);
+            }
+        }
+
+        return depth;
+    }
+}
